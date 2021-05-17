12b73ca12d7ecf6fbefecda4cb816bd32a85506e
jaysmokeglove <jay@vertexcybersecurity.com.au>
2021-05-17 18:28:59 +1000

    Version 5

--- Hashing/secure_hashing.php
+++ Hashing/secure_hashing.php
@@ -7,7 +7,7 @@ $password = "MyPassword!1!";
 $aes256key = hash("SHA256", $password, true); 
 $size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
 $iv = mcrypt_create_iv($size, MCRYPT_DEV_RANDOM);
-$mcrypted=mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $aes256key, $target, MCRYPT_MOD>
+$mcrypted=mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $aes256key, $target, MCRYPT_MODECBC);
 echo base64_encode($mcrypted); 
 echo "<br>";
 echo "Strong Encrypted Text is :";
