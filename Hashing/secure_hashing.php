d6f203b3ee8daad5af1e07b2139d535fbb980383
jaysmokeglove <jay@vertexcybersecurity.com.au>
2021-05-17 18:56:25 +1000

    Version 9

--- Hashing/secure_hashing.php
+++ Hashing/secure_hashing.php
@@ -6,7 +6,7 @@ $target = $_POST["sha"];
 $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
 $size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
 $iv = mcrypt_create_iv($size, MCRYPT_DEV_RANDOM);
-$mcrypted=mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $target, MCRYPT_MODE_CBC);
+$mcrypted=mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $target, MCRYPT_MODE_CBC, $iv);
 echo base64_encode($mcrypted); 
 echo "<br>";
 echo "Strong Encrypted Text is :";
