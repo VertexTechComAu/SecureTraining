a68819da1e315c7152669343460ae964ac759961
jaysmokeglove <jay@vertexcybersecurity.com.au>
2021-05-24 17:25:49 +1000

    Version 15

--- FileUploads/upload.php
+++ FileUploads/upload.php
@@ -1,8 +1,8 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file);
-echo "File Uploaded";
+$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+$file_uploaded=move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file);
+echo "File Uploaded!";
 echo "<pre>";
 print_r ($_FILES);
 echo "</pre>";
