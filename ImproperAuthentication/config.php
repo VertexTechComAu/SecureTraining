2845ee742e69e88bb793970d8c31d408af0e2d3b
jaysmokeglove <84294523+jaysmokeglove@users.noreply.github.com>
2021-05-26 09:21:56 +1000

    Update config.php

--- ImproperAuthentication/config.php
+++ ImproperAuthentication/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'learning_user17');
-define('DB_PASSWORD', 'dT6w8kdovv!mEeaM33');
-define('DB_NAME', 'db17');
+define('DB_USERNAME', 'learning_user##');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'db##');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
