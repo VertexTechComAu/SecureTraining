edcdbd66f807227d9de1551848221f79898822eb
jaysmokeglove <jay@vertexcybersecurity.com.au>
2021-05-26 10:43:29 +1000

    Version 17

--- ImproperAuthentication/config.php
+++ ImproperAuthentication/config.php
@@ -1,16 +1,26 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'learning_user17');
-define('DB_PASSWORD', 'dT6w8kdovv!mEeaM33');
-define('DB_NAME', 'db17');
+<!--?php
+	define("HOST","localhost");
+	define("DB_PASS","");
+	define("DB_USER","");
+	define("DB_NAME","db17");
+	
+	
+	$conn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
+	
+	if(!$conn)
+	{
+		die(mysqli_error());
+	}
  
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+functiongetUserAccessRoleByID($id)
+{
+	global $conn;
+		
+	$query = "select user_role from tbl_user_rolewhere  id = ".$id;
+	
+	$rs = mysqli_query($conn,$query);
+	$row = mysqli_fetch_assoc($rs);
+		
+	return $row['user_role'];
 }
-?>
+?-->
