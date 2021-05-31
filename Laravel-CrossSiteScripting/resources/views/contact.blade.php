a7f55b8adae6e09489e79cd2486c7f2b0bd03bf7
jaysmokeglove <jay@vertexcybersecurity.com.au>
2021-05-31 16:40:47 +1000

    Version 24

---
+++ Laravel-CrossSiteScripting/resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form metho="POST" action="/contact">
+<label for="email">Email: </label>
+<input name="email" type="email" id="email">
+<br>
+<label for="name">Name: </label>
+<input name="name" type="text"id="name">
+<button type="submit">Submit</button>
+</form>
+</body>
+</html>
+
